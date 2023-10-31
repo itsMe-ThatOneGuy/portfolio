b101ad7afac15a84b3daebb19076e394e279695f
Matthew Smart <matthewsmart13@gmail.com>
2023-10-31 11:06:24 -0500

    initial commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import Nav from '../nav/nav';
+
+const Header = () => {
+	return (
+		<header>
+			<div>
+				<p>Matthew Smart</p>
+			</div>
+			<Nav />
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,16 @@
+const Nav = () => {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<button>Resume</button>
+				</li>
+				<li>About</li>
+				<li>Projects</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Nav;
