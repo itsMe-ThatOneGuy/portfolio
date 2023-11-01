7b67759ab87dfddd15bfc2f40d034c7329a25967
Matthew Smart <matthewsmart13@gmail.com>
2023-11-01 00:29:02 -0500

    Basic Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,7 +2,7 @@ import Nav from '../nav/nav';
 
 const Header = () => {
 	return (
-		<header>
+		<header className="flex justify-between border-2 border-black">
 			<div>
 				<p>Matthew Smart</p>
 			</div>
