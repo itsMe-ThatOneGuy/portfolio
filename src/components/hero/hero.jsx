241cf0f1cf9c357dfd90d05a7567f7023c929b55
Matthew Smart <matthewsmart13@gmail.com>
2024-02-06 13:34:26 -0600

    Added tailwind styling to elements

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,11 +1,13 @@
 const Hero = () => {
-	return (
-		<section>
-			<div>
-				<button>Resume</button>
-			</div>
-		</section>
-	);
+    return (
+        <section className="border-2 border-red-900 h-screen">
+            <div className="flex flex-col justify-end items-center min-h-screen">
+                <button className="text-center mb-44 p-3 px-7 text-lg tracking-widest font-bold border-2 border-black rounded-full">
+                    Resume
+                </button>
+            </div>
+        </section>
+    );
 };
 
 export default Hero;
