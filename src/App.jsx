8fb6610673ce27d82236fda1ebc0c40bf02dacda
Matthew Smart <matthewsmart13@gmail.com>
2024-02-06 13:37:37 -0600

    Updated app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react';
 import Header from './components/header/header';
+import Hero from './components/hero/hero';
+import About from './components/about/about';
+import Projects from './components/projects/projects';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
 
 function App() {
-	return (
-		<>
-			<Header />
-		</>
-	);
+    return (
+        <>
+            <Header />
+            <main>
+                <Hero />
+                <About />
+                <Projects />
+                <Contact />
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
