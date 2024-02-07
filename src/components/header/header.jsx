5912f8afd173b45a1899eaeff7b9812c70a23052
Matthew Smart <matthewsmart13@gmail.com>
2024-02-06 23:42:35 -0600

    Removed border from header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,8 +2,8 @@ import Nav from '../nav/nav';
 
 const Header = () => {
 	return (
-		<header className="flex justify-between border-2 border-black">
-			<div>
+		<header className="flex justify-between p-3">
+			<div className="text-2xl font-bold mt-2 ml-5">
 				<p>Matthew Smart</p>
 			</div>
 			<Nav />
