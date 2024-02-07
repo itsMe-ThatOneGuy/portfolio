60a6554c8e684aad0ed601e70df44d602f4cb41b
Matthew Smart <matthewsmart13@gmail.com>
2024-02-06 23:41:50 -0600

    Component tailwindcss style changes

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -4,14 +4,24 @@ import { IoIosMenu } from 'react-icons/io';
 const Nav = () => {
 	return (
 		<nav>
-			<ul className="flex gap-2">
-				<li>
-					<button>Resume</button>
-				</li>
-				<li>About</li>
-				<li>Projects</li>
-				<li>Contact</li>
-			</ul>
+			<div className="text-xl hidden md:flex md:mt-3 md:mr-5">
+				<ul className="flex gap-2">
+					<li className="pr-5">
+						<button>Resume</button>
+					</li>
+					<li className="pr-5">About</li>
+					<li className="pr-5">Projects</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+
+			<div className="mr-5 md:hidden">
+				<IconContext.Provider value={{ size: '3em' }}>
+					<div>
+						<IoIosMenu />
+					</div>
+				</IconContext.Provider>
+			</div>
 		</nav>
 	);
 };
