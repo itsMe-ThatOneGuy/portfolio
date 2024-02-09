1027bc71e2e5341a3104abf8712a4ff84f1ce01e
Matthew Smart <matthewsmart13@gmail.com>
2024-02-09 12:17:47 -0600

    Complete rework / simplification of mobile styling

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -3,58 +3,61 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Hero = () => {
 	return (
-		<section className="h-[calc(100vh-52px)] flex flex-col justify-evenly">
-			<div className="flex flex-col justify-evenly h-3/4">
-				<div className="flex flex-col items-center mb-2">
-					<img
-						className="rounded-full w-52 border-2 border-black drop-shadow-2xl"
-						src="../../../public/profile.jpg"
-					/>
-				</div>
-
-				<div className="flex flex-col justify-evenly items-center px-3 mx-5">
-					<div className="text-center mb-5">
-						<h1 className="text-3xl font-extrabold">
-							Fullstack Developement & Design
-						</h1>
-						<h2 className="text-1xl font-normal pt-3">
-							Solving problems and creating beautiful designs
-						</h2>
+		<section className="h-[calc(100vh-74px)]">
+			<div className="background-container h-full flex flex-col justify-evenly items-center">
+				<div className="content-container flex flex-col items-center">
+					<div className="image-container">
+						<img
+							className="w-52 rounded-full border-2 border-black mb-16"
+							src="../../../public/profile.jpg"
+						/>
 					</div>
-
-					<div className="flex justify-center">
-						<ul className="flex">
-							<IconContext.Provider value={{ size: '3em', color: '#171515' }}>
-								<li className="px-3">
-									<a
-										href="https://github.com/itsMe-ThatOneGuy"
-										rel="noreferrer"
-										target="_blank"
-									>
-										<FaGithub />
-									</a>
-								</li>
-							</IconContext.Provider>
-							<IconContext.Provider value={{ size: '3em', color: '#0A66C2' }}>
-								<li className="px-3">
-									<a
-										href="https://www.linkedin.com/in/matthew-smart13"
-										rel="noreferrer"
-										target="_blank"
-									>
-										<FaLinkedin />
-									</a>
-								</li>
-							</IconContext.Provider>
-						</ul>
+					<div className="content-container text-center">
+						<div className="title-container p-3">
+							<h1 className="text-3xl font-extrabold mb-2">
+								Fullstack Developement & Design
+							</h1>
+							<h2 className="text-1xl font-normal mb-3">
+								Solving Problems by Creating Beautiful Solutions
+							</h2>
+						</div>
+						<div className="link-container">
+							<ul className="flex justify-center gap-5">
+								<IconContext.Provider
+									value={{ size: '2.5em', color: '#171515' }}
+								>
+									<li>
+										<a
+											href="https://github.com/itsMe-ThatOneGuy"
+											rel="noreferrer"
+											target="_blank"
+										>
+											<FaGithub />
+										</a>
+									</li>
+								</IconContext.Provider>
+								<IconContext.Provider
+									value={{ size: '2.5em', color: '#0A66C2' }}
+								>
+									<li>
+										<a
+											href="https://www.linkedin.com/in/matthew-smart13"
+											rel="noreferrer"
+											target="_blank"
+										>
+											<FaLinkedin />
+										</a>
+									</li>
+								</IconContext.Provider>
+							</ul>
+						</div>
 					</div>
 				</div>
-			</div>
-
-			<div className="flex flex-col items-center">
-				<button className="bg-gray-200 text-center p-3 px-7 text-lg tracking-widest font-bold border-2 border-black rounded-full">
-					Resume
-				</button>
+				<div className="button-container">
+					<button className="text-center p-3 px-7 text-lg tracking-widest font-bold border-2 border-black rounded-full">
+						Resume
+					</button>
+				</div>
 			</div>
 		</section>
 	);
