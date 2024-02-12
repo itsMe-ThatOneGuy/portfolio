cbcbdfb7ea1ebfd685bad5c5678cbb67894398a6
Matthew Smart <matthewsmart13@gmail.com>
2024-02-11 23:59:53 -0600

    Added custom color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
-
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+		colors: {
+			purp: '#730071',
+			vpurp: '#390040',
+			grrey: '#827081',
+			plat: '#E6E8E6',
+			dcyan: '#558C8C',
+		},
+	},
+	plugins: [],
+};
