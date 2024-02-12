345140210092ad44a381349cb4bdabd674a29fa5
Matthew Smart <matthewsmart13@gmail.com>
2024-02-12 11:02:25 -0600

    Made header sticky / styling changes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,11 +2,13 @@ import Nav from '../nav/nav';
 
 const Header = () => {
 	return (
-		<header className="flex justify-between p-3">
-			<div className="text-2xl font-bold mt-2 ml-5">
-				<p>Matthew Smart</p>
+		<header className="sticky top-0 z-50">
+			<div className="flex justify-between p-3 bg-grrey opacity-90">
+				<div className="text-2xl font-bold my-1 ml-5">
+					<p className="tracking-wide text-plat">Matthew Smart</p>
+				</div>
+				<Nav />
 			</div>
-			<Nav />
 		</header>
 	);
 };
