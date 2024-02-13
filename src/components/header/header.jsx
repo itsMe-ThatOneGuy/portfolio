3033e6592e3395cc468fc751f7bef91bc1d42ac2
Matthew Smart <matthewsmart13@gmail.com>
2024-02-13 12:57:02 -0600

    Basic styling changes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
 
 	return (
 		<header className="sticky top-0 z-50">
-			<div className="flex justify-between p-3 bg-grrey opacity-90">
+			<div className="flex justify-between p-3 bg-grrey">
 				<div className="text-2xl font-bold my-1 ml-5">
 					<p className="tracking-wide text-plat">Matthew Smart</p>
 				</div>
