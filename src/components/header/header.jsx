01ce8ce9312489862e3d1115bb6b379bd32b615a
Matthew Smart <matthewsmart13@gmail.com>
2024-02-13 12:56:18 -0600

    Added mobile menue logic

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,25 @@
+import { useState } from 'react';
 import Nav from '../nav/nav';
 
 const Header = () => {
+	const [mobileMenu, setMobileMenu] = useState(false);
+
+	const body = document.querySelector('body');
+	const toggleBodyOverflow = () => {
+		!mobileMenu
+			? body.classList.add('active')
+			: body.classList.remove('active');
+	};
+
+	const toggleMobileMenu = () => {
+		setMobileMenu(!mobileMenu);
+		toggleBodyOverflow();
+	};
+
+	const handleClick = () => {
+		toggleMobileMenu();
+	};
+
 	return (
 		<header className="sticky top-0 z-50">
 			<div className="flex justify-between p-3 bg-grrey opacity-90">
