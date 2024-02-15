3f312f81b2778d390984bfb4cf0991c71b5a35b9
Matthew Smart <matthewsmart13@gmail.com>
2024-02-14 23:07:37 -0600

    Basic mobile project section

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,5 +1,15 @@
+import ProjectCard from './project-card';
+
 const Projects = () => {
-	return <section></section>;
+	return (
+		<section className="bg-gradient-to-b from-dcyan to-vpurp h-fit">
+			<div className="p-3 mx-2">
+				<h1 className="text-2xl font-bold text-plat">Projects / Work</h1>
+			</div>
+			<ProjectCard />
+			<ProjectCard />
+		</section>
+	);
 };
 
 export default Projects;
