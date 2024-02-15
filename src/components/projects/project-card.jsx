b6801a713251c24b7b80e224c14134c0c27c037c
Matthew Smart <matthewsmart13@gmail.com>
2024-02-15 00:12:40 -0600

    Reworked project section / cards to loop through data to create

--- src/components/projects/project-card.jsx
+++ src/components/projects/project-card.jsx
@@ -1,5 +1,62 @@
-const ProjectCard = () => {
-	return <div></div>;
+import { IconContext } from 'react-icons';
+import { CiLink } from 'react-icons/ci';
+import { FiGithub } from 'react-icons/fi';
+
+const ProjectCard = (props) => {
+	console.log(props);
+
+	return (
+		<div className="flex flex-col space-y-2 m-6 ">
+			<img
+				width={'343px'}
+				height={'166px'}
+				className="w-full rounded h-64"
+				src={props.project.img}
+			/>
+			<div className="inline-block p-3">
+				<div className="mb-4">
+					<h2 className="text-xl text-plat mb-2 underline decoration-dcyan">
+						{props.project.title}
+					</h2>
+					<h3 className="text-lg text-grrey">{props.project.sub}</h3>
+				</div>
+				<div>
+					<div className="mb-3">
+						<ul className="flex flex-wrap gap-1 text-sm">
+							{props.project.tools.map((e) => {
+								return (
+									<li
+										key={props.project.tools.indexOf(e)}
+										className="p-1 bg-plat"
+									>
+										{e}
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+					<div>
+						<ul className="flex justify-end items-center gap-5">
+							<IconContext.Provider value={{ size: '2.5em' }}>
+								<li className="font-semibold text-grrey">
+									<a href={props.project.live} rel="noreferrer" target="_blank">
+										<CiLink />
+									</a>
+								</li>
+							</IconContext.Provider>
+							<IconContext.Provider value={{ size: '2em' }}>
+								<li className="font-semibold text-grrey">
+									<a href={props.project.repo} rel="noreferrer" target="_blank">
+										<FiGithub />
+									</a>
+								</li>
+							</IconContext.Provider>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ProjectCard;
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,13 +1,17 @@
 import ProjectCard from './project-card';
+import projects from '../../../data/demo-projects';
 
 const Projects = () => {
+	console.log(projects);
+
 	return (
 		<section className="bg-gradient-to-b from-dcyan to-vpurp h-fit">
 			<div className="p-3 mx-2">
 				<h1 className="text-2xl font-bold text-plat">Projects / Work</h1>
 			</div>
-			<ProjectCard />
-			<ProjectCard />
+			{projects.map((e) => {
+				return <ProjectCard key={projects.indexOf(e)} project={e} />;
+			})}
 		</section>
 	);
 };
