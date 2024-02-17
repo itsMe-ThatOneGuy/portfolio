1e1d66492d06b408ec5af2314030029ca104bb27
Matthew Smart <matthewsmart13@gmail.com>
2024-02-17 12:36:22 -0600

    Created / styled  footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,34 @@
+import { IconContext } from 'react-icons';
+import { RiArrowUpDoubleLine } from 'react-icons/ri';
+
 const Footer = () => {
-	return <footer></footer>;
+	return (
+		<footer className="bg-grrey h-fit">
+			<div className="flex flex-col justify-center items-center p-5">
+				<div>
+					<IconContext.Provider value={{ size: '2em', color: '#E6E8E6' }}>
+						<a href="#home">
+							<RiArrowUpDoubleLine />
+						</a>
+					</IconContext.Provider>
+				</div>
+				<div className="p-1">
+					<p className="text-plat font-semibold">
+						<span className="text-vpurp text-lg">©</span> Matthew Smart{' '}
+						<span className="text-vpurp">2024</span>
+					</p>
+				</div>
+				<div className="text-xs p-3">
+					<p>Designed and Developed by me.</p>
+					<p>
+						Coded in <span className="text-vpurp font-bold">NeoVim</span>, built
+						with <span className="text-vpurp font-bold">React.js</span> and{' '}
+						<span className="text-vpurp font-bold">TailwindCSS</span>.
+					</p>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
