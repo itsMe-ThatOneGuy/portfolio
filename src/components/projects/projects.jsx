6118556b71b1bc27df5ee6c0de44ff409e456475
Matthew Smart <matthewsmart13@gmail.com>
2024-02-19 13:45:06 -0600

    Restructure / styling for desktop

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -3,15 +3,17 @@ import projects from '../../../data/demo-projects';
 
 const Projects = () => {
 	return (
-		<section id="projects" className="scroll-m-5">
-			<div className="flex flex-col bg-gradient-to-b from-dcyan to-vpurp h-fit">
-				<div className="p-3 mx-3 mt-10">
-					<h2 className="text-2xl font-bold text-plat">Projects / Work</h2>
-				</div>
-				<div>
-					{projects.map((e) => {
-						return <ProjectCard key={projects.indexOf(e)} project={e} />;
-					})}
+		<section id="projects" className="scroll-m-5 w-full">
+			<div className="flex flex-col items-center bg-gradient-to-b from-dcyan to-vpurp">
+				<div className="lg:w-1/2">
+					<div className="p-3 mx-3 mt-10">
+						<h2 className="text-2xl font-bold text-plat">Projects / Work</h2>
+					</div>
+					<div>
+						{projects.map((e) => {
+							return <ProjectCard key={projects.indexOf(e)} project={e} />;
+						})}
+					</div>
 				</div>
 			</div>
 		</section>
